d1c49e72a2281c8b3a08b0bc0a2a234742643e99
danielmaartens <danielrmaartens@gmail.com>
2019-09-03 13:41:56 +0200

    Added helper filter class to filter for missing numbers

---
+++ app/Util/NumberFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Util;
+
+
+class NumberFilter
+{
+    private $numbers;
+
+    function __construct($numbers)
+    {
+        $this->numbers = $numbers;
+    }
+
+    function filter_missing($value){
+        if (!in_array($value, $this->numbers)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function debugToConsole($msg)
+    {
+        echo "<script>console.log(" . json_encode($msg) . ")</script>";
+    }
+}
